b59d5f61997fbfe1fa3964ab4e5bcc8ddf6c409a
chr-secrect <67987031+chr-secrect@users.noreply.github.com>
2022-06-05 15:28:54 +0800

    Add files via upload

---
+++ Faster-Rcnn/get_dr_txt.py
@@ -0,0 +1,99 @@
+#----------------------------------------------------#
+#   获取测试集的detection-result和images-optional
+#   具体视频教程可查看
+#   https://www.bilibili.com/video/BV1zE411u7Vw
+#----------------------------------------------------#
+import copy
+import os
+
+import numpy as np
+import torch
+import torch.backends.cudnn as cudnn
+from PIL import Image, ImageDraw, ImageFont
+from torch.autograd import Variable
+from torch.nn import functional as F
+from tqdm import tqdm
+
+from frcnn import FRCNN
+from nets.frcnn import FasterRCNN
+from utils.utils import DecodeBox, loc2bbox, nms, get_new_img_size
+
+
+class mAP_FRCNN(FRCNN):
+    #---------------------------------------------------#
+    #   检测图片
+    #---------------------------------------------------#
+    def detect_image(self,image_id,image):
+        self.confidence = 0.01
+        self.iou        = 0.45
+        f = open("./input/detection-results/"+image_id+".txt","w") 
+
+        image_shape = np.array(np.shape(image)[0:2])
+        old_width, old_height = image_shape[1], image_shape[0]
+        old_image = copy.deepcopy(image)
+        
+        #---------------------------------------------------------#
+        #   给原图像进行resize，resize到短边为600的大小上
+        #---------------------------------------------------------#
+        width,height = get_new_img_size(old_width, old_height)
+        image = image.resize([width,height], Image.BICUBIC)
+
+        #-----------------------------------------------------------#
+        #   图片预处理，归一化。
+        #-----------------------------------------------------------#
+        photo = np.transpose(np.array(image,dtype = np.float32)/255, (2, 0, 1))
+
+        with torch.no_grad():
+            images = torch.from_numpy(np.asarray([photo]))
+            if self.cuda:
+                images = images.cuda()
+
+            roi_cls_locs, roi_scores, rois, _ = self.model(images)
+
+            #-------------------------------------------------------------#
+            #   利用classifier的预测结果对建议框进行解码，获得预测框
+            #-------------------------------------------------------------#
+            outputs = self.decodebox.forward(roi_cls_locs[0], roi_scores[0], rois, height = height, width = width, nms_iou = self.iou, score_thresh = self.confidence)
+            #---------------------------------------------------------#
+            #   如果没有检测出物体，返回原图
+            #---------------------------------------------------------#
+            if len(outputs)==0:
+                return old_image
+            outputs = np.array(outputs)
+            bbox = outputs[:,:4]
+            label = outputs[:, 4]
+            conf = outputs[:, 5]
+
+            bbox[:, 0::2] = (bbox[:, 0::2]) / width * old_width
+            bbox[:, 1::2] = (bbox[:, 1::2]) / height * old_height
+            
+        for i, c in enumerate(label):
+            predicted_class = self.class_names[int(c)]
+            score = str(conf[i])
+
+            left, top, right, bottom = bbox[i]
+            f.write("%s %s %s %s %s %s\n" % (predicted_class, score[:6], str(int(left)), str(int(top)), str(int(right)),str(int(bottom))))
+
+        f.close()
+        return 
+
+frcnn = mAP_FRCNN()
+image_ids = open('VOCdevkit/Pest24/ImageSets/Main/val.txt').read().strip().split()
+
+if not os.path.exists("./input"):
+    os.makedirs("./input")
+if not os.path.exists("./input/detection-results"):
+    os.makedirs("./input/detection-results")
+if not os.path.exists("./input/images-optional"):
+    os.makedirs("./input/images-optional")
+
+
+
+for image_id in tqdm(image_ids):
+    image_path = "./VOCdevkit/Pest24/JPEGImages/"+image_id+".jpg"
+    image = Image.open(image_path)
+    # image.save("./input/images-optional/"+image_id+".jpg")
+    frcnn.detect_image(image_id,image)
+    
+
+print("Conversion completed!")
